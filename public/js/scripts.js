9a87b5e8cbce2a04299b25273620efed8ac25c90
TIMUCI <40914849+TIMUCI@users.noreply.github.com>
2019-09-09 12:58:42 +0300

    sucess db connection

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,201 +3,213 @@ window.onload = function () {
 	document.querySelector('#customer-id').focus();
 }
 
-//form element
-var formEl = document.getElementById("toyota-mail");
+
+//computes the cost and displays it in browser
+var computeCost = function () {
+	//get unit price
+	var unitPrice = parseFloat(document.getElementById("unit-price").value),
+		//get cost element
+		costEl = document.getElementById('cost');
+	//if no values provided for the unit price and quantity set them to 0
+	if (unitPrice === undefined || unitPrice === null || isNaN(unitPrice)) unitPrice = 0;
+	if (quantity === undefined || quantity === null || isNaN(quantity)) quantity = 0;
+	//compute cost
+	cost = unitPrice * quantity;
+	//display cost in browser
+	costEl.innerHTML = ` $ ${cost.toFixed(2)}`;
+	// event.preventDefault();
+	return parseFloat(cost);
+
+} //compute cost
 
 
-//get the compute button
-var computeButton = document.getElementById('compute');
-//add event listener
-computeButton.addEventListener("click", (Event) => {
+//computes the sales tax based on town selected
+var computeSalesTax = function () {
+	let taxRate, townCode, retailCustomer, salesTaxEl = document.getElementById("sales-tax");
 
-	var cost, total, quantity, salesTax, shipHandlingFees;
+	// check if checkbox is checked
+	retailCustomer = document.getElementById('retail').checked;
+	//			console.log(retailCustomer);
+	if (!retailCustomer) {
 
-	quantity = parseFloat(document.getElementById('quantity').value);
-
-	//computes the cost and displays it in browser
-	var computeCost = function () {
-		//get unit price
-		var unitPrice = parseFloat(document.getElementById("unit-price").value),
-			//get cost element
-			costEl = document.getElementById('cost');
-
-		//compute cost
-		cost = unitPrice * quantity;
-		//display cost in browser
-		costEl.innerHTML = ` $ ${cost.toFixed(2)}`;
+		salesTax = 0;
+		salesTaxEl.innerHTML = "$ " + salesTax.toFixed(2);
 		// event.preventDefault();
-		return parseFloat(cost);
-
-	} //compute cost
-
-
-	//computes the sales tax based on town selected
-	var computeSalesTax = function () {
-		let taxRate, townCode, retailCustomer, salesTaxEl = document.getElementById("sales-tax");
-
-		// check if checkbox is checked
-		retailCustomer = document.getElementById('retail').checked;
-		//			console.log(retailCustomer);
-		if (!retailCustomer) {
-
-			salesTax = 0;
-			salesTaxEl.innerHTML = "$ " + salesTax.toFixed(2);
-			// event.preventDefault();
-			return salesTax;
-		} else {
-			//get towncodes
-			var selectedCity = document.getElementById('city').value;
-			console.log(selectedCity);
-			// event.preventDefault();
-			//get selected city
-			switch (selectedCity) {
-				//if 'kampala is selected
-				case 'KLA': {
-					salesTax = (cost * 10 / 100).toFixed(2);
-					// event.preventDefault();
-					salesTaxEl.innerHTML = `$ ${salesTax}`;
-					return parseFloat(salesTax);
-					break;
-				}
-				// if entebbe or mbarara are selected
-				case 'EBB':
-				case 'MBR': {
-					salesTax = (cost * 5 / 100).toFixed(2);
-					// event.preventDefault();
-					salesTaxEl.innerHTML = `$ ${salesTax}`;
-					return parseFloat(salesTax);
-					break;
-				}
-				case 'OTH':
-					salesTax = 0;
-					// event.preventDefault();
-					salesTaxEl.innerHTML = `$ ${salesTax}`;
-					return parseFloat(salesTax);
-			} // end switch
-		} //end else
-	} //end compute sales tax
-
-
-	//computes shipping and handling fees
-
-	function shippingHandling() {
-		//shipAndHandle fees
-		var shipAndHandleEl = document.getElementById('ship-bill'),
-			shipping,
-
-			//get shipping methods 
-			shippingMethods = document.getElementsByName('ship_method'),
-			//store the shipping and handling cost
-			shipAndHandle;
-
-		//check if oversize checkbox is checked 
-		var containerOverSize = document.getElementById('oversize').checked;
-		//loop throught the shippingMethods array
-		for (var i = 0; i < shippingMethods.length; i++) {
-
-			//check the checked radio button about shipping methods
-			if (shippingMethods[i].checked) {
-
+		return salesTax;
+	} else {
+		//get towncodes
+		var selectedCity = document.getElementById('city').value;
+		console.log(selectedCity);
+		// event.preventDefault();
+		//get selected city
+		switch (selectedCity) {
+			//if 'kampala is selected
+			case 'KLA': {
+				salesTax = (cost * 10 / 100).toFixed(2);
 				// event.preventDefault();
-				//get value of checked box
-				switch (shippingMethods[i].value) {
-
-					case 'ups': {
-						shipping = 7;
-						break;
-					} //case
-
-					case 'fed_ex_ground': {
-						shipping = 9.25;
-						break;
-					}
-					case 'us_post_air': {
-						shipping = 8.50;
-						break;
-					}
-
-					case 'fed_ex_air': {
-						shipping = 12.00;
-						break;
-					}
-				} //switch end
-				if (!containerOverSize) {
-					shipAndHandle = quantity * shipping;
-					shipAndHandleEl.innerHTML = `$ ${shipAndHandle.toFixed(2)}`;
-					//						console.log(shipAndHandle);
-					// event.preventDefault();
-					return shipAndHandle.toFixed(2);
-				} else {
-					//increment the shipping charge by 5
-					shipping += 5;
-					shipAndHandle = quantity * shipping;
-					shipAndHandleEl.innerHTML = `$ ${shipAndHandle.toFixed(2)}`;
-					//						console.log(shipAndHandle);
-					// event.preventDefault();
-					return shipAndHandle.toFixed(2);
-				} //else end 
-			} //if end 
-		} //forloop
-	} // compute ship handling fees
-
-	// validates form
-	function validData() {
-		//get customer id
-		//get user
-		const customerIdEl = document.querySelector('#customer-id'),
-			userNameEl = document.querySelector('#user-name'),
-			//get user name
-			partNumberEl = document.querySelector('#part_num'),
-			//get description 
-			descriptionEl = document.querySelector('#description'),
-			//get unit price
-			unitPriceEl = document.querySelector("unit-price");
-		//validate customer id 
-		function validateId() {
-			let pattern = /^\d{1,9}$/,
-				validId = pattern.test(customerIdEl.value) && customerIdEl.value !== "";
-			if (validId) {
-				customerIdEl.style = "border-color:gray";
-
-			} else {
-				customerIdEl.style = "border-color:red";
-
+				salesTaxEl.innerHTML = `$ ${salesTax}`;
+				return parseFloat(salesTax);
+				break;
 			}
-			console.log(validId);
+			// if entebbe or mbarara are selected
+			case 'EBB':
+			case 'MBR': {
+				salesTax = (cost * 5 / 100).toFixed(2);
+				// event.preventDefault();
+				salesTaxEl.innerHTML = `$ ${salesTax}`;
+				return parseFloat(salesTax);
+				break;
+			}
+			case 'OTH':
+				salesTax = 0;
+				// event.preventDefault();
+				salesTaxEl.innerHTML = `$ ${salesTax}`;
+				return parseFloat(salesTax);
+		} // end switch
+	} //end else
+} //end compute sales tax
 
-		} //end validateId
-		validateId();
-	} //end validate
+
+//computes shipping and handling fees
+
+function shippingHandling() {
+	//shipAndHandle fees
+	var shipAndHandleEl = document.getElementById('ship-bill'),
+		shipping,
+
+		//get shipping methods 
+		shippingMethods = document.getElementsByName('ship_method'),
+		//store the shipping and handling cost
+		shipAndHandle;
+
+	//check if oversize checkbox is checked 
+	var containerOverSize = document.getElementById('oversize').checked;
+	//loop throught the shippingMethods array
+	for (var i = 0; i < shippingMethods.length; i++) {
+
+		//check the checked radio button about shipping methods
+		if (shippingMethods[i].checked) {
+
+			// event.preventDefault();
+			//get value of checked box
+			switch (shippingMethods[i].value) {
+
+				case 'ups': {
+					shipping = 7;
+					break;
+				} //case
+
+				case 'fed_ex_ground': {
+					shipping = 9.25;
+					break;
+				}
+				case 'us_post_air': {
+					shipping = 8.50;
+					break;
+				}
+
+				case 'fed_ex_air': {
+					shipping = 12.00;
+					break;
+				}
+			} //switch end
+			if (!containerOverSize) {
+				shipAndHandle = quantity * shipping;
+				shipAndHandleEl.innerHTML = `$ ${shipAndHandle.toFixed(2)}`;
+				//						console.log(shipAndHandle);
+				// event.preventDefault();
+				return shipAndHandle.toFixed(2);
+			} else {
+				//increment the shipping charge by 5
+				shipping += 5;
+				shipAndHandle = quantity * shipping;
+				shipAndHandleEl.innerHTML = `$ ${shipAndHandle.toFixed(2)}`;
+				//						console.log(shipAndHandle);
+				// event.preventDefault();
+				return parseFlost(shipAndHandle.toFixed(2));
+			} //else end 
+		} //if end 
+	} //forloop
+} // compute ship handling fees
 
 
-	//computes total
-	function computeTotal() {
+var cost, total, quantity, salesTax = 0;
+var formEl = document.getElementById("toyota-mail");
+// validates form
+function validData() {
+	//form element
 
-		total = (parseFloat(computeCost()) + parseFloat(computeSalesTax()) + parseFloat(shippingHandling())).toFixed(2);
-		//print value to browser
+	quantity = parseFloat(document.getElementById('qty').value);
+	//get customer id
+	//get user
+	const customerIdEl = document.querySelector('#customer-id'),
+		userNameEl = document.querySelector('#user-name'),
+		//get user name
+		partNumberEl = document.querySelector('#part_num'),
+		//get description 
+		descriptionEl = document.querySelector('#description'),
+		//get unit price
+		unitPriceEl = document.querySelector("#unit-price"),
+		quantityEl = document.querySelector("#qty");
 
-		document.getElementById('total').innerHTML = "$ " + total;
-		// event.preventDefault();
-	} //end compute total
+	// function validateId() {
+	//validate customer id 
+	let idPattern = /^\d{1,9}$/,
+		validId = (idPattern.test(customerIdEl.value)) && (customerIdEl.value !== "");
+	document.querySelector("#id-msg").innerHTML = "";
 
-	//compute function call all other functions
-	function compute() {
-		// validData();
-		computeCost();
-		computeSalesTax();
-		shippingHandling();
-		computeTotal();
+	if (!validId) {
+		customerIdEl.style = "border-color:red;";
+		customerIdEl.focus();
+		document.querySelector("#id-msg").innerHTML = "Required and Enter only numbers.";
+		return false;
 	}
-	//call compute()
-	compute();
-}); //event listener
+	customerIdEl.style = "";
+
+	//validate price
+	let validPrice = (!isNaN(unitPriceEl.value)) && (unitPriceEl.value !== "");
+	document.querySelector("#price-msg").innerHTML = "";
+	if (!validPrice) {
+		unitPriceEl.style = "border-color:red";
+		unitPriceEl.focus();
+		document.querySelector("#price-msg").innerHTML = "Price and quantity Can't be missing";
+		return false;
+	}
+	unitPriceEl.style = "";
+
+	//validate quantity
+	let qtyPattern = /^\d+$/,
+		validQty = (qtyPattern.test(quantityEl.value)) && (quantityEl.value.length !== 0);
+	if (!validQty) {
+		quantityEl.style = "border-color:red";
+		quantityEl.focus();
+		return false;
+	}
+	quantityEl.style = "";
+	// return (true);
+
+	computeCost();
+	computeSalesTax();
+	computeTotal();
+	shippingHandling()
+} //end validate
+
+
+//computes total
+function computeTotal() {
+
+	total = (parseFloat(computeCost()) + parseFloat(computeSalesTax()) + parseFloat(shippingHandling())).toFixed(2);
+	//print value to browser
+
+	document.getElementById('total').innerHTML = "$ " + total;
+	// event.preventDefault();
+} //end compute total
+
 
 //closes current window
 function Exit() {
 	let exitButton = document.getElementById('exit');
-	exitButton.addEventListener("click", (event) => {
+	exitButton.addEventListener("click", (Event) => {
 		if (confirm("Are you sure you want to exit?")) {
 			//			close(); //close current window
 			window.close();
@@ -208,7 +220,7 @@ function Exit() {
 //resets the form
 function newOrder() {
 	let newOrderButton = document.getElementById('new-order');
-	newOrderButton.addEventListener("click", (event) => {
+	newOrderButton.addEventListener("click", (Event) => {
 		document.getElementById('toyota-mail').reset();
 	});
 }
--- toyota.js
+++ toyota.js
@@ -22,7 +22,7 @@ app.use(express.static('./public'));
 
 //creating the database connection
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
+    host: 'localhost',
     user: 'root',
     database: 'toyota_db'
 });
@@ -34,12 +34,12 @@ app.get("/", function (req, res) {
 });
 
 //the route for accessing the database
-app.post("/form", (req, res) => {
+app.post("/purchases", (req, res) => {
     const reqBody = req.body;
     //object created to pick the inputs from the form body
     let obj = {
         customer_id: reqBody.customer_id,
-        name: reqBody.user_name,
+        user_name: reqBody.user_name,
         city: reqBody.city,
         part_num: reqBody.part_num,
         unit_price: reqBody.unit_price,
@@ -49,20 +49,21 @@ app.post("/form", (req, res) => {
         retail: reqBody.retail,
         total: reqBody.total
     };
-
-    //assigning variables to get the inputs from the form
+    console.log(obj.customer_id, obj.city);
+    //assigning variables to get the inputs from to he form
     let city = obj.city,
         unit_price = obj.unit_price,
         quantity = obj.quantity,
         retailer = obj.retail,
         ship_method = obj.shipping_method,
-        customerId = obj.customer_id;
+        customerId = obj.customer_id,
+        user_name = obj.user_name;
 
     console.log(customerId, city, unit_price, quantity, retailer, ship_method);
 
     //query for inserting into the database
     const querystring =
-        "INSERT INTO customer_info VALUES(?,?,?,?)";
+        "INSERT INTO customer_info(customer_id, user_name, city, retail) VALUES(?,?,?,?)";
     //using the sql connection to apply the querystring and pass them to the variables
     connection.query(querystring, [
         customerId,
@@ -72,13 +73,11 @@ app.post("/form", (req, res) => {
     ]);
 
     setTimeout(function () {
-        res.redirect('/')
+        res.redirect('/');
         res.end();
-    }, 1000)
-
-
+    }, 5000);
 });
 //notifying the console that the server will listen at port 3000
 
-app.listen(3000);
-console.log('Server is listening from port 3000');
+app.listen(4000);
+console.log('Server is listening from port 4000');
